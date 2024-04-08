fe8c9750b9c8196e0ce7b7d19e951ac4e2edd0a2
shambhav15 <shambhavse15@gmail.com>
2024-04-08 06:10:18 +0530

    bug fix

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,3 @@
 export { default as spinner } from "./spinner";
 export { default as Skeleton } from "./Skeleton";
-export { default as IssueStatusBadge } from "./IssueStatusBadge";
+export { default as IssueStatusBadge } from "./issueStatusBadge";
